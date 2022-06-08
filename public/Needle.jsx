362862adcda51e863412353402df19099f5a9183
Alex <alex.beciana@gmail.com>
2022-06-07 23:05:34 -0400

    Made Needle SVG JSX comp dynamic fill

--- public/Needle.jsx
+++ public/Needle.jsx
@@ -1,5 +1,5 @@
 
-const Needle = ({ fillColor = "" }) => {
+const Needle = ({ fillColor = "coolGray" }) => {
     return (
         <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -9,7 +9,10 @@ const Needle = ({ fillColor = "" }) => {
         <path
             d="M575 75.89S573.26 23 350 23 125 75.89 125 75.89L350 677zM436.91 52.11c18.84 1.65 34.64 3.79 47.85 6.17l-96.48 443-22 58.82 70.65-508z"
             transform="translate(-125 -22.97)"
-        ></path>
+            fill={fillColor}
+        />
         </svg>
     );
 }
+
+export default Needle
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "altWhite": {
+        default: "#f8f8e9"
+      },
+      "coolGray": {
+        default: "#080807"
+      },
+      "royalBlue": {
+        default: "#335ade",
+      },
+      "altGreen": {
+        default: "#558564"
+      },
+      "scarlet": {
+        default: "#ff2e00"
+      },
+      "ceruBlue": {
+        default: "#00a8e8"
+      },
+      "altOrange": {
+        default: "#f0a202"
+      }
+    },
     extend: {},
   },
   plugins: [],
