d005125b2bcc35d83eafbcc59417ccef0f5fd0df
Alex <alex.beciana@gmail.com>
2022-06-09 17:31:53 -0400

    Added sections to homepage && started upcoming events listing

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,27 @@ const Home = () => {
         title="Drop The Needle"
         description="Homepage"
       />
-      <div
-        className="w-10/12 mx-auto py-10"
+      <section>
+        <h1 className="sr-only">Drop The Needle</h1>
+        <div
+          className="w-10/12 mx-auto py-10"
+        >
+          <Image
+            src="/Scripps.jpg"
+            layout="responsive"
+            width={500}
+            height={281}
+          />
+        </div>
+      </section>
+      <section
+        className="mx-20 text-6xl font-medium py-2"
       >
-        <Image
-          src="/Scripps.jpg"
-          layout="responsive"
-          width={500}
-          height={281}
-        />
-      </div>
+        <h2>Upcoming...</h2>
+        <ul
+          className="py-5"
+        ></ul>
+      </section>
     </>
   )
 }
