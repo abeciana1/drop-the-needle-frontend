1ec96ab2bdc225e06f3af575076cf2a3cb72bb86
Alex <alex.beciana@gmail.com>
2022-06-09 23:25:55 -0400

    Added first column image to signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,10 +1,18 @@
 import { TwoColumnGrid } from '../components/core/_layouts'
+import Image from 'next/image'
 
 const Signup = () => {
 
     return (
         <TwoColumnGrid>
-
+            <div>
+                <Image
+                    src="/Sweet Static - April 2020.jpg"
+                    layout="responsive"
+                    width={800}
+                    height={973}
+                />
+            </div>
         </TwoColumnGrid>
     )
 }
