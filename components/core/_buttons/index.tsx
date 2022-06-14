3e6b3db660f6dcea6b34b5f05c6149fa7f091aee
Alex <alex.beciana@gmail.com>
2022-06-14 00:05:16 -0400

    Added solid and outlined button comps

---
+++ components/core/_buttons/index.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import cx from 'classnames'
+
+interface IButtonProps {
+    text: string;
+    href?: string; //* leave optional for none anchor buttons
+    color: string;
+    onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+//! create separate buttons for href and onClick
+
+export const SolidClickButton = ({
+    text,
+    color,
+    onClick
+}: IButtonProps) => {
+    return (
+        <button
+            onClick={onClick}
+            className={cx("px-3 py-1 rounded-md text-3xl", {
+                ["bg-altOrange text-coolGray"]: color === "altOrange",
+                ["bg-altGreen text-altWhite"]: color === "altGreen",
+                ["bg-royalBlue text-altWhite"]: color === "royalBlue",
+                ["bg-ceruBlue text-altWhite"]: color === "ceruBlue",
+                ["bg-scarlet text-altWhite"]: color === "scarlet",
+                ["bg-coolGray text-altWhite"]: color === "coolGray",
+            })}
+        >
+            { text }
+        </button>
+    )
+}
+
+export const SolidHrefButton = ({
+    text,
+    color,
+    href
+}: IButtonProps) => {
+    return (
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener"
+        >
+            <button
+                className={cx("px-3 py-2 rounded-md text-3xl", {
+                    ["bg-altOrange text-coolGray"]: color === "altOrange",
+                    ["bg-altGreen text-altWhite"]: color === "altGreen",
+                    ["bg-royalBlue text-altWhite"]: color === "royalBlue",
+                    ["bg-ceruBlue text-altWhite"]: color === "ceruBlue",
+                    ["bg-scarlet text-altWhite"]: color === "scarlet",
+                    ["bg-coolGray text-altWhite"]: color === "coolGray",
+                })}
+            >
+                { text }
+            </button>
+        </a>
+    )
+}
+
+export const OutlineClickButton = ({
+    text,
+    color,
+    onClick
+}: IButtonProps) => {
+    return (
+        <button
+            onClick={onClick}
+                className={cx("bg-altWhite px-3 py-1 rounded-md text-3xl", {
+                    ["border-4 border-altOrange text-altOrange"]: color === "altOrange",
+                    ["border-4 border-altGreen text-altGreen"]: color === "altGreen",
+                    ["border-4 border-royalBlue text-royalBlue"]: color === "royalBlue",
+                    ["border-4 border-ceruBlue text-ceruBlue"]: color === "ceruBlue",
+                    ["border-4 border-scarlet text-scarlet"]: color === "scarlet",
+                    ["border-4 border-coolGray text-coolGray"]: color === "coolGray",
+                })}
+        >
+            { text }
+        </button>
+    )
+}
+
+export const OutlineHrefButton = ({
+    text,
+    color,
+    href
+}: IButtonProps) => {
+    return (
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener"
+        >
+            <button
+                className={cx("bg-altWhite px-3 py-1 rounded-md text-3xl", {
+                    ["border-4 border-altOrange text-altOrange"]: color === "altOrange",
+                    ["border-4 border-altGreen text-altGreen"]: color === "altGreen",
+                    ["border-4 border-royalBlue text-royalBlue"]: color === "royalBlue",
+                    ["border-4 border-ceruBlue text-ceruBlue"]: color === "ceruBlue",
+                    ["border-4 border-scarlet text-scarlet"]: color === "scarlet",
+                    ["border-4 border-coolGray text-coolGray"]: color === "coolGray",
+                })}
+            >
+                { text }
+            </button>
+        </a>
+    )
+}
