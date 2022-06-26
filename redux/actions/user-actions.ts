ee99761fb1238c41c932e7124587a572f7730182
Alex <alex.beciana@gmail.com>
2022-06-26 13:56:33 -0400

    Created user action contants and imported in reducer with initial state

---
+++ redux/actions/user-actions.ts
@@ -0,0 +1,6 @@
+//! user action constants
+export const USER_SIGNUP = "USER_SIGNUP"
+export const USER_LOGIN = "USER_LOGIN"
+export const USER_LOGOUT = "USER_LOGOUT"
+export const USER_UPDATE = "USER_UPDATE"
+export const USER_CHECK_LOG_TOKEN = "USER_CHECK_LOG_TOKEN"
---
+++ redux/reducers/user-reducer.ts
@@ -0,0 +1,12 @@
+import {
+    USER_SIGNUP,
+    USER_LOGIN,
+    USER_LOGOUT,
+    USER_UPDATE,
+    USER_CHECK_LOG_TOKEN
+} from '../actions/user-actions'
+
+const initialState = {
+    currentUser: null
+}
+
