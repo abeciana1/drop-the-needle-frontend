591f46117006fea7cd18d5565a81f097df68a989
Alex <alex.beciana@gmail.com>
2022-06-26 22:01:41 -0400

    Removed commented out imports and inital implementation

--- redux/store.ts
+++ redux/store.ts
@@ -1,25 +1,3 @@
-// import { createStore, applyMiddleware, compose, CombinedState } from 'redux'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-// const composeEnhancer = compose<any>(
-//     composeWithDevTools(applyMiddleware(thunk))
-// )
-
-// const reducer = rootReducer
-
-    // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-
-// export const store = (context: any) => createStore(
-//     rootReducer,
-//     composeEnhancer
-// )
-// import { createWrapper } from 'next-redux-wrapper'
-// interface State {
-//   tick: string;
-// }
-// const wrapper = createWrapper<Store<State>>(store, {debug: false})
-// export default wrapper
-
-
 import rootReducer from '../redux/reducers'
 import thunk from 'redux-thunk'
 import { configureStore } from '@reduxjs/toolkit'
