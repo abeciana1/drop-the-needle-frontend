d04f59e02cc4dce892f9d3b325a2e9233287f414
Alex <alex.beciana@gmail.com>
2022-08-03 00:28:29 -0400

    Started render of power hour playlist on archives page

--- pages/listen/index.tsx
+++ pages/listen/index.tsx
@@ -1,14 +1,36 @@
 import CustomHead from '../../components/core/CustomHead'
+import { ThreeColumnGrid } from '../../components/core/_layouts'
+import PlaylistCard from '../../components/_containers/PlaylistCard'
+
+//* tasks
+// todo - add heading
+// todo - map content to layout
+// todo - add animation to power hours based on mouse enter/leave with vinyl record
 
 const ListenIndex = (props: any) => {
     const { powerHours } = props
-
+    console.log(powerHours);
     return (
         <>
             <CustomHead
                 title="Drop The Needle | Archives"
                 description="Listen to past Power Hours"
             />
+            <div className="text-center pb-20 font-medium">
+                <h1>Archives</h1>
+                <h2 className="py-4">Listen to past Power Hours</h2>
+            </div>
+            <ThreeColumnGrid>
+                {powerHours.map((playlist: any) => {
+                    return (
+                    <PlaylistCard
+                        title={playlist['title']}
+                        description={playlist['description']}
+                        timestamp={playlist['date_time']}
+                        coverImage={playlist['cover_image']}
+                    />)
+                })}
+            </ThreeColumnGrid>
         </>
     )
 }
