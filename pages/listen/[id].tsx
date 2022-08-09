a56c517413e3df31a0ff02a0be467b24040b7293
Alex <alex.beciana@gmail.com>
2022-08-09 17:31:36 -0400

    Added getStaticPaths and getStaticProps data fetching for dynamic listen pages

---
+++ pages/listen/[id].tsx
@@ -0,0 +1,41 @@
+
+const PlaylistPresent = (props: any) => {
+    console.log(props);
+    return (
+        <>
+        </>
+    )
+}
+
+export const getStaticPaths = async() => {
+    let paths: any[] = []
+
+    const res = await fetch("http://localhost:3001/api/v1/power_hours")
+
+    const data = await res.json()
+
+    data.map((playlist: any) => {
+        paths.push({
+            params: {
+                id: playlist.id.toString()
+            }
+        })
+    })
+
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+export const getStaticProps = async (context: any) => {
+    const { params } = context
+    const renderedPlaylist = await fetch("http://localhost:3001/api/v1/power_hours/" + params.id).then(res => res.json())
+    return {
+        props: {
+            renderedPlaylist
+        }
+    }
+}
+
+export default PlaylistPresent
