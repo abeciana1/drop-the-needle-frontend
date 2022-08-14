4112517fc89697ec85750393edce616acf43c7a2
Alex <alex.beciana@gmail.com>
2022-08-14 09:37:40 -0400

    Added order number to song select field

--- components/_forms/inputs/index.tsx
+++ components/_forms/inputs/index.tsx
@@ -119,14 +119,14 @@ export const SongSelectField = ({
     selectedValue,
     setSelectedValue
 }: SingleSelectFieldI) => {
-
+    console.log(selectedValue);
     return (
         <div className="w-72">
         <label className="sr-only">{ labelText }</label>
         <Listbox value={selectedValue} onChange={setSelectedValue}>
             <div className="relative mt-1">
             <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                <span className="block truncate">{selectedValue[property]}</span>
+                <span className="block truncate">{`${selectedValue.order_number} — ${selectedValue[property]}`}</span>
                 <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <SelectorIcon
                     className="h-5 w-5 text-gray-400"
