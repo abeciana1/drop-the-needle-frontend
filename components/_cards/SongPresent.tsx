f0a68e697bb11f83466665d9f464a1b4f985e69c
Alex <alex.beciana@gmail.com>
2022-09-12 16:15:00 -0400

    installed react-youtube and uninstalled react-player

--- components/_cards/SongPresent.tsx
+++ components/_cards/SongPresent.tsx
@@ -1,6 +1,8 @@
 // import { useEffect, useState } from 'react'
-import ReactPlayer from 'react-player/lazy'
-const debounce = require('lodash.debounce')
+// import ReactPlayer from 'react-player/lazy'
+import YouTube from 'react-youtube';
+
+// const debounce = require('lodash.debounce')
 
 interface SongPresentI {
     title: string;
@@ -28,13 +30,14 @@ const SongPresent = ({
     handleEnding
 }: SongPresentI) => {
 
+    console.log('link', link)
+
 {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/fvXPmtkojnw?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
 
     return (
         <>
             <div className="h-56 md:h-72 lg:h-[32rem]">
-                <ReactPlayer
-                    muted={false}
+                {/* <YouTube
                     url={link}
                     loop={false}
                     controls={true}
@@ -42,7 +45,7 @@ const SongPresent = ({
                     height="100%"
                     onEnded={debounce(handleEnding, 2000)}
                     playing={currentSongIdx >= 1}
-                />
+                /> */}
             </div>
             <section className="text-center py-5">
                 <h5 className="leading-relaxed">#{ order_number }. {title} - {artist} — {album}</h5>
