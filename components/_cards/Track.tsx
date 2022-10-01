e7af26d370c2c535316c43710a7fb8ee1052fb80
Alex <alex.beciana@gmail.com>
2022-10-01 15:53:47 -0400

    Made track list and track comps responsive

--- components/_cards/Track.tsx
+++ components/_cards/Track.tsx
@@ -41,13 +41,13 @@ const Track = ({
 
     return (
         <li
-            className="relative flex w-full border-coolGray px-2 py-4"
+            className="relative flex flex-col md:flex-row w-full border-coolGray px-2 py-4"
         >
             <div className="font-medium">#{stateOrderNumber}</div>
             <div className="ml-2 font-medium">Title: <span className="font-normal">{title}</span></div>
-            <div className="absolute left-1/3 ml-2 font-medium v">Album: <span className="font-normal">{album}</span></div>
+            <div className="md:absolute md:left-1/3 ml-2 font-medium v">Album: <span className="font-normal">{album}</span></div>
             <ExpandBtn
-                addClass="absolute right-10 bottom-2 font-medium"
+                addClass="absolute right-5 bottom-1/3 xs:right-10 sm:bottom-2 font-medium"
                 onClick={removeTrackHandler}
                 icon={XIcon}
                 text="Remove"
