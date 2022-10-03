61fd2d5c01797fd77adaeadfe1c90ded7621643b
Alex <alex.beciana@gmail.com>
2022-10-03 12:30:19 -0400

    Added switch case statement for playlist and songs on playlist reducer && set up initial playlist actions file

---
+++ redux/actions/playlist-actions.ts
@@ -0,0 +1,11 @@
+//! playlist action constants
+export const SET_PLAYLIST = "SET_PLAYLIST"
+export const SET_SONGS = "SET_SONGS"
+
+const API_URL = 'http://localhost:3001/api/v1/'
+
+// export const setPlaylist = (playlistId: number) => {
+//     return (dispatch: any) => {
+//         fetch(API_URL + "power")
+//     }
+// }
--- redux/reducers/playlist-reducer.ts
+++ redux/reducers/playlist-reducer.ts
@@ -1,4 +1,7 @@
-
+import {
+    SET_PLAYLIST,
+    SET_SONGS
+} from '../actions/playlist-actions'
 import { PayloadAction } from '@reduxjs/toolkit'
 
 interface PlaylistInterface {
@@ -14,7 +17,18 @@ const playlistReducer = (
     state = initialState,
     action: PayloadAction<any>
 ) => {
-
+    switch (action.type) {
+        case SET_PLAYLIST:
+            return {
+                ...state,
+                playlist: action.payload
+            }
+        case SET_SONGS:
+            return {
+                ...state,
+                songs: action.payload
+            }
+    }
 }
 
 export default playlistReducer
