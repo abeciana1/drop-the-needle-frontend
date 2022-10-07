014f9c6b56bf389c4e3c1e83992a6db517dceb87
Alex <alex.beciana@gmail.com>
2022-10-07 15:24:06 -0400

    Added additional playlist actions && created draft deleteSong action

--- redux/actions/playlist-actions.ts
+++ redux/actions/playlist-actions.ts
@@ -1,8 +1,14 @@
 //! playlist action constants
-export const SET_PLAYLIST = "SET_PLAYLIST"
-export const SET_SONGS = "SET_SONGS"
 export const REVERT_PLAYLIST = "REVERT_PLAYLIST"
 export const REVERT_SONGS = "REVERT_SONGS"
+export const SET_SONGS = "SET_SONGS"
+export const DELETE_SONG = "DELETE_SONG"
+
+//! playlist CRUD actions for power hour
+export const SET_PLAYLIST = "SET_PLAYLIST"
+export const CREATE_PLAYLIST = "CREATE_PLAYLIST"
+export const DELETE_PLAYLIST = "DELETE_PLAYLIST"
+export const UPDATE_PLAYLIST = "UPDATE_PLAYLIST"
 
 const API_URL = 'http://localhost:3001/api/v1/'
 
@@ -36,3 +42,21 @@ export const revertSongs = () => {
         })
     }
 }
+
+// todo TEST THIS
+// export const deleteSong = (songId: number) => {
+//     return (dispatch: any) => {
+//         const const options = {
+//             method: 'DELETE',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json',
+//             }
+//         }
+//         fetch(API_URL + "songs/" + songId, options)
+//             .then(res => res.json())
+//             .then(data => {
+//             console.log(data)
+//         })
+//     }
+// }
